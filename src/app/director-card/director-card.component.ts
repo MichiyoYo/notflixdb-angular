24d6a296cfed267c15f92c54c30289fea8773c83
Cristina Lester <cristina.deelester@gmail.com>
2021-11-07 15:21:25 +0100

    added director dialog

--- src/app/director-card/director-card.component.ts
+++ src/app/director-card/director-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director-card',
   templateUrl: './director-card.component.html',
-  styleUrls: ['./director-card.component.scss']
+  styleUrls: ['./director-card.component.scss'],
 })
 export class DirectorCardComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      name: string;
+      bio: string;
+      birthDate: any;
+      deathDate: any;
+    }
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
