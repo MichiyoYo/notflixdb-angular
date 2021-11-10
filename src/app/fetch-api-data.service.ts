39985f45b0844e99d9738be029368ab139edf3c5
Cristina Lester <cristina.deelester@gmail.com>
2021-11-10 18:15:15 +0100

    adding comments to api services

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,3 +1,8 @@
+/**
+ * Collection of methods to perform calls to NotFlix API endopoints
+ * @module fetch-api-data
+ */
+
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/internal/operators';
 import {
@@ -21,6 +26,11 @@ export class FetchApiDataService {
     this.http = http;
   }
 
+  /**
+   * Handles the error passed as argument
+   * @param error the error to handle
+   * @returns the handled error
+   */
   private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
       console.error('Some error occurred:', error.error.message);
@@ -33,7 +43,11 @@ export class FetchApiDataService {
     return throwError('Something bad happened, please try again later. 💔');
   }
 
-  // Non-typed response extraction
+  /**
+   * Non-typed response extraction
+   * @param res the response to extrac
+   * @returns the body of the response
+   */
   private extractResponseData(res: any): any {
     const body = res;
     return body || {};
@@ -41,7 +55,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the user registration endpoint
-   * @function userRegistration
    * @param userDetails the payload of the request
    * @returns an Observable containing a response
    */
@@ -53,7 +66,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the /login endpoint
-   * @function userLogin
    * @param userDetails the payload of the request
    * @returns an Observable containing a response
    */
@@ -71,7 +83,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the /movies endpoint
-   * @function getAllMovies
    * @returns an Observable containing a response
    */
   public getAllMovies(): Observable<any> {
@@ -89,62 +100,7 @@ export class FetchApiDataService {
   }
 
   /**
-   * Calls the /catalog/genres endpoint
-   * @function getAllGenres
-   * @returns an Observable containing a response
-   */
-  public getAllGenres(): Observable<any> {
-    const token = localStorage.getItem('token');
-    const response = this.http.get(apiUrl + 'catalog/genres', {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + token,
-      }),
-    });
-    return response.pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
-  }
-
-  /**
-   * Calls the /catalog/actors endpoint
-   * @function getAllActors
-   * @returns an Observable containing a response
-   */
-  public getAllActors(): Observable<any> {
-    const token = localStorage.getItem('token');
-    const response = this.http.get(apiUrl + 'catalog/actors', {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + token,
-      }),
-    });
-    return response.pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
-  }
-
-  /**
-   * Calls the /catalog/directors endpoint
-   * @function getAllActors
-   * @returns an Observable containing a response
-   */
-  public getAllDirectors(): Observable<any> {
-    const token = localStorage.getItem('token');
-    const response = this.http.get(apiUrl + 'catalog/directors', {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + token,
-      }),
-    });
-    return response.pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
-  }
-
-  /**
    * Calls the /movies/:movieTitle endpoint
-   * @function getMovie
    * @param movieTitle the id of the movie to retrieve
    * @returns an Observable containing a response
    */
@@ -163,7 +119,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the /directors/:directornName endpoint
-   * @function getDirector
    * @param directorNane the name of the actor to retrieve
    * @returns an Observable containig a response
    */
@@ -184,27 +139,7 @@ export class FetchApiDataService {
   }
 
   /**
-   * Calls the /actors/:actorName endpoint
-   * @function getActor
-   * @param actorName the name of the actor to retrieve
-   * @returns an Observable containing a response
-   */
-  public getActor(actorName: string): Observable<any> {
-    const token = localStorage.getItem('token');
-    const response = this.http.get(apiUrl + 'catalog/actors/' + actorName, {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + token,
-      }),
-    });
-    return response.pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
-  }
-
-  /**
    * Calls the /genres/:genreName endpoint
-   * @function getGenre
    * @param genreName the name of the genre to retrieve
    * @returns an Observable conianing a response
    */
@@ -223,7 +158,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the /users/:username endpoint
-   * @function getUser
    * @param username the name of the user to retrieve
    * @returns an Observable conianing a response
    */
@@ -242,7 +176,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the /users/username/favorites endpoint
-   * @function getFavMovies
    * @param username the username of the user to retrieve the favorite movies of
    * @returns an Observable containing a response
    */
@@ -263,31 +196,7 @@ export class FetchApiDataService {
   }
 
   /**
-   * Calls the /:users/username/favorites endpoint
-   * @function getWatchlist
-   * @param username the username of the user to retrieve the list of fvaorites of
-   * @returns an Observable containing a response
-   */
-  public getWatchlist(username: string): Observable<any> {
-    const token = localStorage.getItem('token');
-    const response = this.http.get(
-      apiUrl + 'users/' + username + '/watchlist',
-      {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      }
-    );
-
-    return response.pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
-  }
-
-  /**
    * Calls the PUT /users/:username/favorites/:movieId endpoint
-   * @function addToFav
    * @param username the username of the user we want to update the favorites for
    * @param movieId the id of the movie we want to add to the favorites
    * @returns an Observable containing a response
@@ -312,34 +221,7 @@ export class FetchApiDataService {
   }
 
   /**
-   * Calls the PUT /users/:username/favorites/:movieId endpoint
-   * @function addToWatchlist
-   * @param username the username of the user we want to update the favorites for
-   * @param movieId the id of the movie we want to add to the favorites
-   * @returns an Observable containing a response
-   */
-  public addToWatchlist(username: string, movieId: string): Observable<any> {
-    const token = localStorage.getItem('token');
-
-    const response = this.http.post(
-      apiUrl + 'users/' + username + 'watchlist/' + movieId,
-      {},
-      {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-        responseType: 'text',
-      }
-    );
-    return response.pipe(
-      map(this.extractResponseData),
-      catchError(this.handleError)
-    );
-  }
-
-  /**
    * Calls the DELETE /users/:username/favorites/:movieId endpoint
-   * @function removeFromFav
    * @param username the username of the user we want to update the favorites for
    * @param movieId the id of the movie we want to remove from favorites
    * @returns an Observable containing a response
@@ -362,31 +244,7 @@ export class FetchApiDataService {
   }
 
   /**
-   * Calls the DELETE /users/:username/favorites/:movieId endpoint
-   * @function removeFromWatchlist
-   * @param username the username of the user we want to update the favorites for
-   * @param movieId the id of the movie we want to add to the favorites
-   * @returns an Observable containing a response
-   */
-  public removeFromWatchlist(
-    username: string,
-    movieId: string
-  ): Observable<any> {
-    const token = localStorage.getItem('token');
-    const response = this.http.delete(
-      apiUrl + 'users/' + username + 'watchlist/' + movieId,
-      {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      }
-    );
-    return response.pipe(catchError(this.handleError));
-  }
-
-  /**
    * Calls the /users/:username/deregister endpoint
-   * @function deleteUser
    * @param username the username of the user we want to deregister
    * @returns an Observable containing a response
    */
@@ -405,7 +263,6 @@ export class FetchApiDataService {
 
   /**
    * Calls the PUT /users/:username
-   * @function editUser
    * @param username the user we want to update the info of
    * @param updatedInfo the new info
    * @returns an Observable containing a response
